9b6b934811ce678f3385c39e9f8bafb06712c2fe
Jan Steffen <jan.steffen@finleap.com>
2022-10-20 14:32:21 +0200

    fix
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 
 USB_DEVICE=$(bashio::config 'device')
 bashio::log.info "Using usb device ${USB_DEVICE}"
@@ -6,4 +6,4 @@ bashio::log.info "Using usb device ${USB_DEVICE}"
 # start vcontrold
 # https://github.com/openv/vcontrold/blob/master/doc/man/vcontrold.rst
 bashio::log.info "Starting vcontrold.."
-vcontrold -x /config/vcontrold.xml --nodaemon
+vcontrold -x /etc/vcontrold/vcontrold.xml --nodaemon
