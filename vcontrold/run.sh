8885223e99299eb5888bf9eb2e817dc164051fda
Jan Steffen <jan.steffen@finleap.com>
2022-10-20 14:43:40 +0200

    fix
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
+CONFIG_PATH=/data/options.json
+
 USB_DEVICE=$(bashio::config 'device')
 bashio::log.info "Using usb device ${USB_DEVICE}"
+
+# adjust config to use configured device
+sed -i -e "/<serial>/,/<\/serial>/ s|<tty>[0-9a-z\/._A-Z:]\{1,\}</tty>|<tty>$USB_DEVICE</tty>|g" /etc/vcontrold/vcontrold.xml
 
 # start vcontrold
 # https://github.com/openv/vcontrold/blob/master/doc/man/vcontrold.rst
