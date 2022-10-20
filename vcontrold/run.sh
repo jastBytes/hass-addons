e95070787b52ab338c3a186ae11cd6e619eaec42
Jan Steffen <jan.steffen@finleap.com>
2022-10-20 16:10:51 +0200

    fix config
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -4,12 +4,11 @@ set -e
 CONFIG_PATH=/data/options.json
 
 USB_DEVICE=$(bashio::config 'device')
+PORT="$(bashio::addon.port 3002)"
 bashio::log.info "Using usb device ${USB_DEVICE}"
-
-# adjust config to use configured device
-sed -i -e "/<serial>/,/<\/serial>/ s|<tty>[0-9a-z\/._A-Z:]\{1,\}</tty>|<tty>$USB_DEVICE</tty>|g" /etc/vcontrold/vcontrold.xml
+bashio::log.info "Using port ${PORT}"
 
 # start vcontrold
 # https://github.com/openv/vcontrold/blob/master/doc/man/vcontrold.rst
 bashio::log.info "Starting vcontrold.."
-vcontrold -x /etc/vcontrold/vcontrold.xml --nodaemon
+vcontrold -x /etc/vcontrold/vcontrold.xml --nodaemon -h $USB_DEVICE -p $PORT
