48e52b3b12d16c06f9da065e13bdbab20ad85e2f
Jan Steffen <jan.steffen@finleap.com>
2022-10-20 13:58:50 +0200

    simplify
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- vcontrold/config/mqtt_publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-jq -c '.[]' result.json | while read i; do
-    COMMAND=$(echo $i | jq -r ' .command')
-    RAW=$(echo $i | jq -r ' .value')
-    PAYLOAD=$(echo $i | jq -r '. ')
-    mosquitto_pub -u $MQTTUSERNAME -P $MQTTPASSWORD -h $MQTTHOST -p $MQTTPORT -t $MQTTTOPIC/$COMMAND -m "$PAYLOAD" -x 120 -c --id "VCONTROLD-PUB" -V "mqttv5"
-done
