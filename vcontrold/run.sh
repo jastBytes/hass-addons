14ab884c165fd6f7c5e9fb2d7f406ecaaff00cae
Jan Steffen <jan.steffen@finleap.com>
2022-10-20 14:22:17 +0200

    enhance
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-sleep 3                 # search the USB device as it sometimes is USB0 or USB1
-USB_DEVICE=/dev/vitocal # USB_DEVICE=`find /dev/ -name vitocal*`
 
-echo "Device ${USB_DEVICE}"
-
-# make device accessable
-chmod 777 ${USB_DEVICE}
+USB_DEVICE=$(bashio::config 'device')
+bashio::log.info "Using usb device ${USB_DEVICE}"
 
 # start vcontrold
 # https://github.com/openv/vcontrold/blob/master/doc/man/vcontrold.rst
-echo "Starting vcontrold..."
+bashio::log.info "Starting vcontrold.."
 vcontrold -x /config/vcontrold.xml --nodaemon
